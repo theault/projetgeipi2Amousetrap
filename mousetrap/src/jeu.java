c70f4e6aaa10669025b89e24822dc812f8ec91d8
theault <garridotheault@gmail.com>
2014-03-21 20:43:17 +0100

    plus que le sprite pi j'affiche

--- mousetrap/src/jeu.java
+++ mousetrap/src/jeu.java
@@ -1,14 +1,30 @@
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 
 public class jeu extends Game {
 
-	/**
-	 * @param args
-	 */
+	BufferedImage Mouse;
+	String imagemouse;
+	
 	public void begin () {
 		GameApplication.start(new jeu());
 
+	}
+	
+	public jeu ()
+	{
+		//imagemouse= "wesh l'adresse
+		/*try{
+		Mouse=ImageIO.read(new File (imagemouse));
+		} catch (IOException e){
+			e.printStackTrace();
+		}*/
+		
 	}
 
 	@Override
@@ -19,8 +35,7 @@ public class jeu extends Game {
 
 	@Override
 	public void draw(Graphics2D g) {
-		// TODO Auto-generated method stub
-		
+		g.drawImage(Mouse.getSubimage(x, y, w, h), 100,100,20,20, null);// pour afficher le sprite de la souris 
 	}
 
 }
